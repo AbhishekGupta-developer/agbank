ce8e9ec75fa3bba773476a9be443e839051ec336
Abhishek Gupta <abhishekg.developer@gmail.com>
2024-09-27 19:15:15 +0530

    Add @After annotation in BankAspect

--- src/main/java/com/myorganisation/agbank/aspect/BankAspect.java
+++ src/main/java/com/myorganisation/agbank/aspect/BankAspect.java
@@ -1,7 +1,7 @@
 package com.myorganisation.agbank.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,8 +14,16 @@ public class BankAspect {
     Logger logger = LoggerFactory.getLogger(BankAspect.class);
 
     @Before("execution(* com.myorganisation.agbank.service.BankService.viewAccount(..))")
-    public void printLogStatementsBefore() {
+    public void printLogStatementsViewAccountBefore() {
         logger.info("viewAccount() method called!");
+        logger.info("Start time: " + System.currentTimeMillis());
+    }
+
+    @After("execution(* com.myorganisation.agbank.service.BankService.viewAccount(..))")
+    public void printLogStatementsViewAccountAfter() {
+        logger.info("viewAccount() method completed!");
+        logger.info("End time: " + System.currentTimeMillis());
+
     }
 
 }
