2cb1aaa197b34f0efd50f988f2031a3cfaf35ec6
Abhishek Gupta <abhishekg.developer@gmail.com>
2024-09-30 03:01:27 +0530

    Modify complete code of BankAspect

--- src/main/java/com/myorganisation/agbank/aspect/BankAspect.java
+++ src/main/java/com/myorganisation/agbank/aspect/BankAspect.java
@@ -1,10 +1,14 @@
 package com.myorganisation.agbank.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Aspect
 @Component
@@ -13,35 +17,52 @@ public class BankAspect {
     //creating logger here
     Logger logger = LoggerFactory.getLogger(BankAspect.class);
 
-    @Before("execution(* com.myorganisation.agbank.service.BankService.viewAccount(..))")
-    public void printLogStatementsViewAccountBefore() {
-        logger.info("viewAccount() method called!");
-        logger.info("Start time: " + System.currentTimeMillis());
+    @Pointcut("execution(* com.myorganisation.agbank.controller.BankController.*(..))")
+    public void loggingPointcut() {}
+
+    @Before("loggingPointcut()")
+    public void beforeAdviceLoggingPointcut(JoinPoint joinPoint) {
+        logger.info("@Before advice: {} method invoked! at {}",
+                joinPoint.getSignature().getName(),
+                DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now())
+        );
+        logger.info("JoinPoint: {}", joinPoint.getSignature());
     }
 
-    @After("execution(* com.myorganisation.agbank.service.BankService.viewAccount(..))")
-    public void printLogStatementsViewAccountAfter() {
-        logger.info("viewAccount() method completed!");
-        logger.info("End time: " + System.currentTimeMillis());
-
+    @After("loggingPointcut()")
+    public void afterAdviceLoggingPointcut(JoinPoint joinPoint) {
+        logger.info("@After advice: {} method invoked! at {}",
+                joinPoint.getSignature().getName(),
+                DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now())
+        );
+        logger.info("JoinPoint: {}", joinPoint.getSignature());
     }
 
-    @Pointcut(value = "execution(* com.myorganisation.agbank.service.BankService.checkBalance(..))")
-    public void printLogStatementsCheckBalancePointcut() {
+    @Around("loggingPointcut()")
+    public Object aroundAdviceLoggingPointcut(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        logger.info("@Around advice: before invocation of {} method! at {}",
+                proceedingJoinPoint.getSignature().getName(),
+                DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now())
+        );
+        logger.info("JoinPoint: {}", proceedingJoinPoint.getSignature());
 
-    }
-
-    @Around(value = "printLogStatementsCheckBalancePointcut()")
-    public void aroundAdvice(ProceedingJoinPoint jp) throws Throwable {
-        System.out.println("The method aroundAdvice() before invokation of  the method " + jp.getSignature().getName() + " method");
-
+        //Proceed with the actual method invocation
+        Object result;
         try {
-            jp.proceed();
-        } finally {
+            result = proceedingJoinPoint.proceed();
+        } catch (Exception e) {
+            logger.info("Exception occurred: {}", e.getMessage());
+            throw e;
+        } finally {}
 
-        }
+        logger.info("@Around advice: after invocation of {} method! at {}",
+                proceedingJoinPoint.getSignature().getName(),
+                DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now())
+        );
+        logger.info("JoinPoint: {}", proceedingJoinPoint.getSignature());
 
-        System.out.println("The method aroundAdvice() after invokation of  the method " + jp.getSignature().getName() + " method");
+        //Return the result to the caller
+        return result;
     }
 
 }
